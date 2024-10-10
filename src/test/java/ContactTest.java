68ed4e18a760d06030fe2d398ece87a6c68e7e84
Carlos Bracho <brachocarlos152@gmail.com>
2024-10-10 17:08:05 -0400

    Renamed some test, changed some comments, so they make more sense. Also, created some tests for Invalid Phone Number and Address when they are Null.

--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -25,7 +25,7 @@ class ContactTest {
     // Test with invalid data when contactID = Null
     @Test
     public void testInvalidContactID_Null() {
-        // Expect IllegalArgumentException when contactID is too long
+        // Expect IllegalArgumentException when contactID is Null
         assertThrows(IllegalArgumentException.class, () -> {
             new Contact(null, "Camille", "Shaine", "0987654321", "123 Pallet Town");
         });
@@ -34,6 +34,7 @@ class ContactTest {
     // Test with invalid data when contactID = Too Long
     @Test
     public void testInvalidContactID_TooLong() {
+        // Expect IllegalArgumentException when contactID is Too Long
         assertThrows(IllegalArgumentException.class, () -> {
             new Contact("12345678901", "Camille", "Shaine", "0987654321", "123 Pallet Town");
         });
@@ -51,6 +52,7 @@ class ContactTest {
     // Test with invalid First Name = Too Long
     @Test
     public void testInvalidFirstName_TooLong() {
+        // Expect IllegalArgumentException when firstName is Too Long
         assertThrows(IllegalArgumentException.class, () -> {
             new Contact("1234567890", "Camille Shaine", "Shaine", "0987654321", "123 Pallet Town");
         });
@@ -68,27 +70,45 @@ class ContactTest {
     // Test with invalid Last Name = Too Long
     @Test
     public void testLastName_TooLong() {
-        // Expect IllegalArgumentException when lastName is null
+        // Expect IllegalArgumentException when lastName is Too Long
         assertThrows(IllegalArgumentException.class, () -> {
             new Contact("1234567890", "Camille", "Shaine Bracho Test", "0987654321", "123 pallet Town");
         });
     }
 
-    // Test with invalid Phone
+    // Test with invalid Phone = Too Short
     @Test
-    public void testInvalidPhoneNumber() {
-        // Expect IllegalArgumentException when phone number is not 10 digits
+    public void testInvalidPhoneNumber_TooShort() {
+        // Expect IllegalArgumentException when phone number is not exactly 10 digits
         assertThrows(IllegalArgumentException.class, () -> {
             new Contact("1234567890", "Camille", "Shaine", "12345", "123 Pallet Town");
         });
     }
 
-    // Test with invalid Address
+    // Test with invalid Phone = Null
     @Test
-    public void testAddressTooLong() {
+    public void testInvalidPhoneNumber_Null() {
+        // Expect IllegalArgumentException when phone number is Null
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Contact("1234567890", "Camille", "Shaine", null, "123 Pallet Town");
+        });
+    }
+
+    // Test with invalid Address = Too Long
+    @Test
+    public void testInvalidAddress_TooLong() {
         // Expect IllegalArgumentException when address is longer than 30 characters
         assertThrows(IllegalArgumentException.class, () -> {
             new Contact("1234567890", "Camille", "Shaine", "0987654321", "123 Pallet Town 12345678901234567890");
+        });
+    }
+
+    // Test with invalid Address = null
+    @Test
+    public void testInvalidAddress_Null() {
+        // Expect IllegalArgumentException when address is longer than 30 characters
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Contact("1234567890", "Camille", "Shaine", "0987654321", null);
         });
     }
 
